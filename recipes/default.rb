043f92f4792f6f67bb72cc8a27c9c2556b2961b4
root <lianxingye>
2018-08-23 14:58:54 +0000

    add all files

---
+++ recipes/default.rb
@@ -0,0 +1,41 @@
+execute "apt-get-update" do
+  command "apt-get update"
+end
+
+execute "install" do
+  command "apt-get install -y erlang"
+end
+
+package 'gnupg'
+
+execute "download rabbit" do
+  command "wget 'https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc' -P /tmp"
+end
+
+execute "download rabbit" do
+  command "apt-key add /tmp/rabbitmq-release-signing-key.asc"
+end
+
+execute "configure source list" do
+  command "echo 'deb https://dl.bintray.com/rabbitmq/debian bionic erlang' > /etc/apt/sources.list.d/bintray.erlang.list"
+end
+
+execute "configure source list 2" do
+  command "echo 'deb https://dl.bintray.com/rabbitmq/debian bionic main' >> /etc/apt/sources.list.d/bintray.erlang.list"
+end
+
+package 'rabbitmq-server'
+
+execute "add web" do
+  command "rabbitmq-plugins enable rabbitmq_management"
+end
+
+service "rabbitmq-server" do
+  action :restart
+end
+
+execute "test" do
+  command "curl -w ' <-- We downloaded %{size_download} bytes\n' -i -u guest:guest http://localhost:15672/api/whoami"
+  live_stream true
+  action :run
+end
